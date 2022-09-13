5647732536ba3da46086fde7aa357fbdc14c6987
André <andremessias.az@gmail.com>
2022-09-13 16:44:02 -0300

    setup: setup static web landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,8 @@
-import './styles/main.css'
+import { MagnifyingGlassPlus } from 'phosphor-react'
 
-import logoImg from './assets/logo-nlw.svg'
+import "./styles/main.css";
+
+import logoImg from "./assets/logo-nlw.svg";
 
 function App() {
   return (
@@ -8,10 +10,40 @@ function App() {
       <img src={logoImg} alt="" />
 
       <h1 className="text-6xl text-white font-black mt-20">
-        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui.
       </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {['1', '2', '3', '4', '5', '6'].map(item => (
+          <a key={item} href='' className="relative rounded-lg overflow-hidden">
+            <img src={`/game-${item}.png`} alt="" />
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+              <strong className="font-bold text-white block">Counter Strike</strong>
+              <span className="text-zinc-300 text-sm mt-1">4 anúncios</span>
+            </div>
+          </a>
+        ))}
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="block text-2xl text-white font-black">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400">Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex justify-center items-center gap-3">
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
