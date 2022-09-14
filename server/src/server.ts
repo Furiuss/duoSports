4a65d4f2ab4e591bc50be634e257023823ddc15e
André <andremessias.az@gmail.com>
2022-09-14 01:02:39 -0300

    setup: initial server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,29 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get("/games", (req, res) => {
+  return res.json([]);
+});
+
+app.post("/ads", (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  // const gameId = request.params.id;
+
   return response.json([
-    {id: 1, name: 'Anuncio 1'},
-    {id: 2, name: 'Anuncio 2'},
-    {id: 3, name: 'Anuncio 3'},
-  ])
+    { id: 1, name: "Anuncio 1" },
+    { id: 2, name: "Anuncio 2" },
+    { id: 3, name: "Anuncio 3" },
+  ]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  // const ad = request.params.id;
+
+  return response.json([])
 })
 
-app.listen(3333)
+app.listen(3333);
