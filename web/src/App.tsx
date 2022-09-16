f3bb7b759e69bf2c854e9193a38be257c1832b92
André <andremessias.az@gmail.com>
2022-09-16 15:39:16 -0300

    fix: fix main app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,7 +6,8 @@ import { CreateAdBanner } from "./components/CreateAdBanner";
 import logoImg from "./assets/logo-nlw.svg";
 
 import "./styles/main.css";
-import { CreateAdForm } from "./components/CreateAdForm";
+import { CreateAdModal } from "./components/CreateAdModal";
+
 
 interface Game {
   id: string;
@@ -54,17 +55,7 @@ function App() {
       <Dialog.Root>
         <CreateAdBanner />
 
-        <Dialog.Portal>
-          <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
-
-          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-black/25">
-            <Dialog.Title className="text-3xl font-black">
-              Publique um anúncio
-            </Dialog.Title>
-            
-              <CreateAdForm />
-          </Dialog.Content>
-        </Dialog.Portal>
+        <CreateAdModal />
       </Dialog.Root>
     </div>
   );
