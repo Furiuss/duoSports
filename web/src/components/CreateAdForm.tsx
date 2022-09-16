d2a8b5fbb1d41dbe8b1c9caa2da29683ae694039
André <andremessias.az@gmail.com>
2022-09-16 15:47:44 -0300

    update: style the checkbox using radix!

--- web/src/components/CreateAdForm.tsx
+++ web/src/components/CreateAdForm.tsx
@@ -1,5 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { GameController } from "phosphor-react";
+import * as Checkbox from "@radix-ui/react-checkbox";
+
+import { Check, GameController } from "phosphor-react";
 import { Input } from "./Form/Input";
 
 export function CreateAdForm() {
@@ -59,8 +61,12 @@ export function CreateAdForm() {
         </div>
       </div>
 
-      <div>
-        <Input type="checkbox" />
+      <div className="mt-2 flex items-center gap-2 text-sm">
+        <Checkbox.Root className="w-6 h-6 p-1 rounded bg-zinc-900">
+          <Checkbox.Indicator>
+            <Check className="w-4 h-4 text-emerald-400" />
+          </Checkbox.Indicator>
+        </Checkbox.Root>
         Costumo me conectar ao chat de voz
       </div>
 
